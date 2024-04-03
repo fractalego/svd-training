a16e5babe42227e33a1cffe171f38ebbc80377fa
Alberto Cetoli <albertocetoli@gmail.com>
2024-04-03 23:56:42 +0100

    testing generation and merge_all()

--- src/svd_model.py
+++ src/svd_model.py
@@ -1,7 +1,10 @@
-import logging
 import torch
+import logging
 
-from transformers import MistralForCausalLM
+from functools import lru_cache
+from typing import Mapping, Any
+from transformers import MistralForCausalLM, MistralConfig
+from src.variables import get_mlp_names, get_norm_names
 
 _logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -19,17 +22,22 @@ class SVDLinear(torch.nn.modules.module.Module):
         self.sigma = torch.nn.Parameter(sigma, requires_grad=True)
         self.V = torch.nn.Parameter(V, requires_grad=False)
         self.weight = torch.nn.Parameter(weight, requires_grad=False)
+        self.U.data = self.U.data.contiguous()
+        self.sigma.data = self.sigma.data.contiguous()
+        self.V.data = self.V.data.contiguous()
+        self.weight.data = self.weight.data.contiguous()
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return input @ self._get_svd_weight()
+        return input @ self._get_svd_weight().T
 
     def get_merged_linear(self):
         linear = torch.nn.Linear(self.weight.shape[1], self.weight.shape[0], bias=False)
-        linear.weight = self.weight + self._get_svd_weight()
+        linear.weight = torch.nn.Parameter(self._get_svd_weight(), requires_grad=True)
         return linear
 
+    @lru_cache
     def _get_svd_weight(self):
-        return self.U @ torch.diag(self.sigma) @ self.V.T
+        return self.weight + self.U @ torch.diag(self.sigma) @ self.V.T
 
     @staticmethod
     def create_from_weight(weight, rank_fraction=0.1, niter=2):
@@ -41,23 +49,16 @@ class SVDLinear(torch.nn.modules.module.Module):
 
 
 class SVDMistralForCausalLM(MistralForCausalLM):
-    mlp_names = [
-        "mlp.down_proj",
-        "mlp.up_proj",
-        "mlp.gate_proj",
-        "self_attn.q_proj",
-        "self_attn.k_proj",
-        "self_attn.v_proj",
-        "self_attn.o_proj",
-    ]
+    mlp_names = get_mlp_names()
     norm_names = [
         "input_layernorm",
         "post_attention_layernorm",
     ]
 
-    def __init__(self, model, rank_fraction):
+    def __init__(self, model):
         super().__init__(model.config)
-        self._build_from_model(model, rank_fraction)
+        self.model = model.model
+        self.lm_head = model.lm_head
 
     def merge_all(self):
         for layer_index in range(len(self.model.layers)):
@@ -66,29 +67,51 @@ class SVDMistralForCausalLM(MistralForCausalLM):
                     f"self.model.layers[layer_index].{mlp_name} = self.model.layers[layer_index].{mlp_name}.get_merged_linear()"
                 )
         self.lm_head = self.lm_head.get_merged_linear()
-        self.model.embed_tokens = self.model.embed_tokens.get_merged_linear()
 
-    def _build_from_model(self, model, rank_fraction):
-        _logger.info(f"Building SVD model with rank_fraction={rank_fraction}")
-        _logger.info(f"lm_head and embed_tokens are substituted with rank_fraction={rank_fraction}")
-        self.lm_head = SVDLinear.create_from_weight(model.lm_head.weight, rank_fraction)
-        self.model.embed_tokens = SVDLinear.create_from_weight(
-            model.model.embed_tokens.weight, rank_fraction
+    @staticmethod
+    def create_from_state_dict(state_dict: Mapping[str, Any]):
+        model_name = "mistralai/Mistral-7B-Instruct-v0.1"
+        config = MistralConfig.from_pretrained(model_name)
+        model = MistralForCausalLM(config)
+        model.model.norm.weight = torch.nn.Parameter(state_dict["model.norm.weight"])
+        model.model.norm.weight = torch.nn.Parameter(state_dict["model.norm.weight"])
+        model.model.embed_tokens.weight = torch.nn.Parameter(
+            state_dict["model.embed_tokens.weight"]
         )
+        for layer_index in range(len(model.model.layers)):
+            for mlp_name in get_mlp_names():
+                weight = state_dict[f"model.layers.{layer_index}.{mlp_name}.weight"]
+                U = state_dict[f"model.layers.{layer_index}.{mlp_name}.U"]
+                sigma = state_dict[f"model.layers.{layer_index}.{mlp_name}.sigma"]
+                V = state_dict[f"model.layers.{layer_index}.{mlp_name}.V"]
+                exec(
+                    f"model.model.layers[layer_index].{mlp_name} = SVDLinear(U, sigma, V, weight)"
+                )
+            for norm_name in get_norm_names():
+                weight_name = f"model.layers.{layer_index}.{norm_name}.weight"
+                exec(
+                    f"model.model.layers[layer_index].{norm_name}.weight = torch.nn.Parameter(state_dict['{weight_name}'])"
+                )
+        weight = state_dict[f"lm_head.weight"]
+        U = state_dict[f"lm_head.U"]
+        sigma = state_dict[f"lm_head.sigma"]
+        V = state_dict[f"lm_head.V"]
+        model.lm_head = SVDLinear(U, sigma, V, weight)
+        return SVDMistralForCausalLM(model)
+
+    @staticmethod
+    def create_from_model(model, rank_fraction):
+        _logger.info(f"Building SVD model with rank_fraction={rank_fraction}")
+        _logger.info(f"lm_head is substituted with rank_fraction={rank_fraction}")
+        model.lm_head = SVDLinear.create_from_weight(model.lm_head.weight, rank_fraction)
         for layer_index in range(len(model.base_model.layers)):
-            for mlp_name in self.mlp_names:
+            for mlp_name in get_mlp_names():
                 exec(f"weight = model.model.layers[layer_index].{mlp_name}.weight")
                 exec(
-                    f"self.model.layers[layer_index].{mlp_name} = SVDLinear.create_from_weight(weight, rank_fraction)"
+                    f"model.model.layers[layer_index].{mlp_name} = SVDLinear.create_from_weight(weight, rank_fraction)"
                 )
                 _logger.info(
                     f"Layer {layer_index} on {mlp_name} with rank_fraction={rank_fraction} is substituted"
                 )
 
-        _logger.info(f"Copying norms from the original model to the new one")
-        self.model.norm.weight = model.model.norm.weight
-        for layer_index in range(len(model.base_model.layers)):
-            for norm_name in self.norm_names:
-                exec(
-                    f"self.model.layers[layer_index].{norm_name} = model.model.layers[layer_index].{norm_name}"
-                )
+        return model
--- tests/test_mistral.py
+++ tests/test_mistral.py
@@ -1,31 +1,41 @@
+import os
 import unittest
+import torch
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
 from src.svd_model import SVDMistralForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
 model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
-svd_model = SVDMistralForCausalLM(model, rank_fraction=0.25)
+original_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+_filename = "mistral_svd_model.psd"
+if os.path.exists(_filename):
+    del model
+    svd_model = SVDMistralForCausalLM.create_from_state_dict(torch.load(_filename))
+
+else:
+    svd_model = SVDMistralForCausalLM.create_from_model(model, rank_fraction=0.25)
+    torch.save(svd_model.state_dict(), _filename)
+    del model
 
 
 class TestMistral(unittest.TestCase):
-    def test_mistral_can_be_loaded(self):
+    def test_1_mistral_can_be_loaded(self):
         self.assertIsNotNone(svd_model)
 
-    def test_trainable_parameters_count(self):
+    def test_2_trainable_parameters_count(self):
         total_params = sum(p.numel() for p in svd_model.parameters() if p.requires_grad)
-        expected_params = 448_512
+        expected_params = 131_519_488
         self.assertEqual(expected_params, total_params)
 
-    def test_generation(self):
-        input_ids = tokenizer.encode("Hello, my name is")
-        output = svd_model.generate(input_ids, max_length=20)
+    def test_3_generation(self):
+        input_ids = tokenizer.encode("Hello, my name is", return_tensors="pt")
+        output = svd_model.generate(input_ids, max_length=12)
         print(tokenizer.decode(output[0]))
         self.assertIsNotNone(output)
 
-    def test_merge(self):
-        original_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-        merged_model = svd_model.merge_all()
-        merged_trainable_params = sum(p.numel() for p in merged_model.parameters() if p.requires_grad)
+    def test_4_merge(self):
+        svd_model.merge_all()
+        merged_trainable_params = sum(p.numel() for p in svd_model.parameters() if p.requires_grad)
         self.assertEqual(original_trainable_params, merged_trainable_params)
-
