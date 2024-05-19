184610f88b60e481780c6d64acccc716aae935b0
Alberto Cetoli <albertocetoli@gmail.com>
2024-05-19 13:06:06 +0100

    using different causalLM for different models

--- svd_training/model_keys.py
+++ svd_training/model_keys.py
@@ -21,6 +21,17 @@ def get_mlp_names(model_name: str) -> List[str]:
             "self_attn.o_proj",
         ]
 
+    if "llama-3" in model_name.lower():
+        return [
+            "mlp.down_proj",
+            "mlp.up_proj",
+            "mlp.gate_proj",
+            "self_attn.q_proj",
+            "self_attn.k_proj",
+            "self_attn.v_proj",
+            "self_attn.o_proj",
+        ]
+
 
 def get_norm_names(model_name) -> List[str]:
     if model_name == "mistralai/Mistral-7B-Instruct-v0.1":
@@ -35,10 +46,16 @@ def get_norm_names(model_name) -> List[str]:
             "post_attention_layernorm",
         ]
 
+    if "llama-3" in model_name.lower():
+        return [
+            "input_layernorm",
+            "post_attention_layernorm",
+        ]
 
 def supported_models() -> List[str]:
     return [
         "mistralai/Mistral-7B-Instruct-v0.1",
         "microsoft/Phi-3-mini-128k-instruct",
         "microsoft/Phi-3-mini-4k-instruct",
+        "meta-llama/Meta-Llama-3-8B-Instruct",
     ]
